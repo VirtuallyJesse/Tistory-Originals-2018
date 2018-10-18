6c3014f35ce3c41ce24c9a39435fa33fdbd715cc
VirtuallyJesse <VirtuallyJesse@users.noreply.github.com>
2018-10-17 19:55:18 -0500

    Update to include older URL style support.

--- Tistory Originals 2018.user.js
+++ Tistory Originals 2018.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tistory Originals 2018
 // @namespace    https://github.com/VirtuallyJesse/Tistory-Originals-2018
-// @version      1.0
+// @version      1.1
 // @description  Redirect tistory image urls to the original resolution
 // @author       VirtuallyJesse
 // @include      *://t1.daumcdn.net/cfile/tistory/*
